93749990582616ccfbf2720962d9cc6edfc0cd21
rowana16 <aaron.rowan@gmail.com>
2018-12-21 19:53:33 -0500

    Add project files.

---
+++ FileSwitcherConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSwitcherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
